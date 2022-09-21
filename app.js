05291b62b49f9b10fce691fadcc2e02527171809
unknown <titimaseko.123@gmail.com>
2022-09-21 14:08:28 +0200

    modify app.js file to start connecting to mongo database and import the user model in it

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 let express = require("express");
 let app = express();
 let bodyParser = require("body-parser");
+let homepageRouter = require("./routes/index"); // import the starting page router
+let mongoose = require("mongoose"); // import the mongoose dependency
+mongoose.connect("mongodb://localhost:27017/demo_thatsAuthentic"); // connect to localhost database
 
-let homepageRouter = require("./routes/index");
+let User = require("./models/user.js"); // import the user model
 
 app.use(homepageRouter);
 
