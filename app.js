1029a8741f3164eef9f00452fc0a7c176580de0c
unknown <titimaseko.123@gmail.com>
2022-09-28 19:37:19 +0200

    clean up app

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-// initialise the app with express
-let express = require("express");
+// Importing dependencies/packages
+let express = require("express"); // initialise the app with express
 let bodyParser = require("body-parser");
 let passport = require("passport");
 let passportLocal = require("passport-local");
 let passportLocalMongoose = require("passport-local-mongoose");
 let expressSession = require("express-session");
 let mongoose = require("mongoose"); // import the mongoose dependency
-mongoose.connect("mongodb://localhost:27017/demo_thatsAuthentic"); // connect to localhost database
+mongoose.connect("mongodb://localhost:27017/demo_thatsAuthentic"); // connect to a local database (for now)
 
-// models
+// Importing models
 let User = require("./models/user.js"); // import the user model
 
-// exporting routes
+// Importing routes
 let homepageRouter = require("./routes/index"); // import the starting page router
 let signupRouter = require("./routes/signup");
 
@@ -34,7 +34,7 @@ app.use(homepageRouter);
 app.use(signupRouter);
 
 
-// initialize this port to test code in google chrome or postman 
+// Initialize this port to test code in google chrome or postman 
 app.listen(3344, function(){
     console.log("ThatsAuthentic Server Has Started")
 });
