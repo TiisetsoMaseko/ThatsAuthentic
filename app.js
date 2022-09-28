85cb409b469417e3a93cc0e552fb8139c47653fa
unknown <titimaseko.123@gmail.com>
2022-09-28 10:08:08 +0200

    include authentication setup in the app

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 // initialise the app with express
 let express = require("express");
-let app = express();
 let bodyParser = require("body-parser");
 let passport = require("passport");
 let passportLocal = require("passport-local");
 let passportLocalMongoose = require("passport-local-mongoose");
+let expressSession = require("express-session");
 let homepageRouter = require("./routes/index"); // import the starting page router
 let mongoose = require("mongoose"); // import the mongoose dependency
 mongoose.connect("mongodb://localhost:27017/demo_thatsAuthentic"); // connect to localhost database
 
 let User = require("./models/user.js"); // import the user model
+let app = express();
 
+app.use(expressSession({
+    secret: "Nails enhance beauty, that is why you come to ThatsAuthentic nail salon",
+    resave: false,
+    saveUninitialized: false
+}));
+app.set("view engine", "ejs");
+app.use(passport.initialize()); //use the passport in the app
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser()); // reading session, taking user datae and ncoding user data
+passport.deserializeUser(User.deserializeUser()); // decoding user data
 app.use(homepageRouter);
 
 
