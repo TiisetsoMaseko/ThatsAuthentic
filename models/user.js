647c5b0495da61e328ab135c0c4ca19aebd5729b
unknown <titimaseko.123@gmail.com>
2022-09-28 10:08:47 +0200

    add authentication to user model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 let mongoose = require("mongoose"); // import mongoose
 
+let passportLocalMongoose = require("passport-local-mongoose");
 // create a user schema with user properties
 let userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose); // add authentication to the user
 module.exports = mongoose.model("User", userSchema); //return the user model object
