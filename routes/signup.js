2c06f87ce930fd1e77f9637403884dc1e505f93c
unknown <titimaseko.123@gmail.com>
2022-10-03 13:10:10 +0200

    add sign up routing(incomplete)

--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,23 @@
 let express = require("express");
 let router = express.Router();
+const user = require("../models/user");
 
+router.get("/signup/new", function(req, res){
+    res.render("signup");
+});
 
-router.get("/signup", function(req, res){
-    res.send("sign up form goes here")
+router.post("/signup", function(req,res){
+    //grab the user profile from the form and put to database
+    
+    let data = req.body.signup;
+    // user.create(data, function(err, User){
+    //     if (err){
+    //         console.log("something went wrong");
+    //     }
+    //     else{
+    //         console.log("Successfully added user to db");
+    //     }
+    // });
 });
 
 
