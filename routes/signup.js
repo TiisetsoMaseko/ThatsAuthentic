7e9b33c9b2533b4b3e6a71921c2d16ee02c53972
unknown <titimaseko.123@gmail.com>
2022-10-04 10:12:30 +0200

    modify sign up functionality to incorporate authentiction

--- routes/signup.js
+++ routes/signup.js
@@ -1,24 +1,29 @@
-let express = require("express");
-let router = express.Router();
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
 const user = require("../models/user");
 
+// send the sign up form to user
 router.get("/signup", function(req, res){
     res.render("signup");
 });
 
+//grab the user profile from the form and put to database
 router.post("/signup", function(req,res){
-    //grab the user profile from the form and put to database
-    
     let data = req.body.signup;
     console.log(data);
-    // user.create(data, function(err, User){
-    //     if (err){
-    //         console.log("something went wrong");
-    //     }
-    //     else{
-    //         console.log("Successfully added user to db");
-    //     }
-    // });
+    user.register(new user({username: data.username, email: data.email}), data.password, function(err, User){
+        if (err){
+            console.log(err);
+            res.render("signup");
+        }
+        else{
+            // log the user in the system
+            passport.authenticate("local")(req,res, function(){ // using "local" strategy
+                res.send("Here user will see all available services");
+            });
+        }
+    });
 });
 
 
