f2b86841edf39cbe31acfe2a619d3957ef427225
unknown <titimaseko.123@gmail.com>
2022-10-19 11:52:43 +0200

    add image attribute to service object

--- routes/services.js
+++ routes/services.js
@@ -4,9 +4,9 @@ let router = express.Router();
 
 router.get("/services", function(req, res){
     let services = [
-        {serviceType: "Full Set Acrylic", price: 240, time: "2hrs"},
-        {serviceType: "Nail Art", price: 100, time: "5mins"},
-        {serviceType: "Just Gel", price: 190, time: "1hr"}
+        {serviceType: "Full Set Acrylic", price: 240, time: "2hrs", img: "../images/full-set-acrylic.jpg"},
+        {serviceType: "Nail Art", price: 100, time: "5mins", img: "../images/full-set-acrylic+nail-art.jpg"},
+        {serviceType: "Just Gel", price: 190, time: "1hr", img: "../images/just-gel.jpg"}
     ];
     res.render("services", {services: services});
 });
