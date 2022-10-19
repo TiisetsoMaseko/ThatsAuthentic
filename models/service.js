f8c7484d9b28b4f97b146b27af3672c3ecdc397c
unknown <titimaseko.123@gmail.com>
2022-10-19 14:07:36 +0200

    add img attribute in the service object

--- models/service.js
+++ models/service.js
@@ -4,7 +4,8 @@ let mongoose = require("mongoose");
 let serviceSchema = new mongoose.Schema({
     serviceType: String,
     price: Number,
-    time: String
+    time: String,
+    img: String
 });
 
 module.exports = mongoose.model("Service", serviceSchema);
