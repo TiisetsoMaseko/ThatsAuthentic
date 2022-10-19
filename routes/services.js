3166de544ad3c8c93d8cb34d78f59e209ca8fb60
unknown <titimaseko.123@gmail.com>
2022-10-19 14:08:44 +0200

    find services in database and parse to services page

--- routes/services.js
+++ routes/services.js
@@ -1,14 +1,17 @@
 let express = require("express");
 let router = express.Router();
+let Service = require("../models/service");
 
 
 router.get("/services", function(req, res){
-    let services = [
-        {serviceType: "Full Set Acrylic", price: 240, time: "2hrs", img: "../images/full-set-acrylic.jpg"},
-        {serviceType: "Nail Art", price: 100, time: "5mins", img: "../images/full-set-acrylic+nail-art.jpg"},
-        {serviceType: "Just Gel", price: 190, time: "1hr", img: "../images/just-gel.jpg"}
-    ];
-    res.render("services", {services: services});
+    Service.find({},function(err, services){
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.render("services",{services, services});
+        }
+    });
 });
 
 module.exports = router;
