5ab4a98fb5dd1a1d7944f34e828867a88d3172d9
unknown <titimaseko.123@gmail.com>
2022-10-20 10:58:23 +0200

    include middleware to view services

--- routes/services.js
+++ routes/services.js
@@ -1,9 +1,10 @@
 let express = require("express");
 let router = express.Router();
 let Service = require("../models/service");
+let middleware = require("../middleware/middleware");
 
 
-router.get("/services", function(req, res){
+router.get("/services", middleware.isLoggedIn, function(req, res){
     Service.find({},function(err, services){
         if (err){
             console.log(err);
